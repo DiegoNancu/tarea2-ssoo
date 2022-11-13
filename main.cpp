d9f15c1a739b1d92b433ffad226d251ab8804f4b
DiegoNancu <d_nancupillan@hotmail.com>
2022-11-13 16:50:34 -0300

    fix

--- main.cpp
+++ main.cpp
@@ -9,13 +9,17 @@ using namespace std;
 mutex myMutex;
 string pckg;
 
+//realiza la verificacion del estado de la ip
+//parametros: ip
 void ipCheck(string ip);
 
+//retorna la cantidad de lineas que tiene el archivo .txt
+//parametros: nombre del archivo .txt
 int ipCount(string ipfile);
 
 int main(int argc, char *argv[]) {
 	pckg = argv[2];
-	//obtener ips
+	//recuparacion de ips del archivo .txt
 	string filename = argv[1];
 	int ipcount = ipCount(filename);
 	string iplist[ipcount];
@@ -27,28 +31,29 @@ int main(int argc, char *argv[]) {
 		iplist[ip] = aux;
 		ip++;
 	}
-	
 	file.close();
-	
+	//definicion de los threads
 	thread threads[ipcount];
-	
+	//base de la tabla de resultados
 	cout << "IP\t\tTrans.\tRec.\tPerd.\tEstado" << endl;
+	//creacion de los threads
 	for(int i = 0; i < ipcount; i++) {
 		threads[i] = thread(ipCheck, iplist[i]);
 	}
-
+	//uso de los threads
 	for(int i = 0; i < ipcount; i++) {
 		threads[i].join();
 	}
-	
 	return 0;
 }
 
 void ipCheck(string ip) {
+	//armado del comando a usar
+	string command = "ping " + ip + " -c " + pckg;
+	//definicion de variables necesarias para el uso de popen
 	char buffer[128];
 	string aux = "";
-	string command = "ping " + ip + " -c " + pckg;
-	
+	//uso de pipe para la obtencion del resultado del uso del comando	
 	FILE *pipe = popen(command.c_str(), "r");
 	if(!pipe) cout << "error" << endl;
 	while(!feof(pipe)) {
@@ -56,7 +61,8 @@ void ipCheck(string ip) {
 			aux += buffer;
 	}
 	pclose(pipe);
-	
+	//recuperacion de los datos guardados en la variable aux
+	//busqueda de los paquetes recibidos
 	string receive = "";
 	int comma = 0;
 	for(int i = 0; i < aux.length(); i++) {
@@ -64,10 +70,13 @@ void ipCheck(string ip) {
 		if(comma == 1 && aux[i] == ' ') break;
 		if(comma == 1) receive += aux[i];
 	}
+	//definicion del estado de la conexion
 	string status;
 	if(stoi(receive) > 0) status = "UP";
 	else status = "DOWN";
+	//definicion de la cantidad de paquetes perdidos
 	int lost = stoi(pckg) - stoi(receive);
+	//muestra en pantalla de los resultados
 	myMutex.lock();
 	if(ip.length() < 10)
 	cout << ip << "\t\t" << pckg << "\t" << receive << "\t" << lost << "\t" << status << endl;
@@ -78,13 +87,11 @@ void ipCheck(string ip) {
 
 int ipCount(string ipfile) {
 	ifstream file(ipfile.c_str());
-	
 	string aux;
 	int count = 0;
 	while(getline(file, aux)) {
 		count++;
 	}
-	
 	file.close();
 	return count;
 }
